971b6e85917b0c11a3a5e5148086890542e40c51
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2025-03-26 20:45:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,22 @@ const express = require("express");
 const math = require("mathjs");
 
 const app = express();
-app.use(express.json()); // JSON body parsing ke liye
+app.use(express.json());
 
-// Evaluate API: Any math expression
+// Function to replace √ with sqrt()
+function formatExpression(expression) {
+    return expression.replace(/√(\d+)/g, "sqrt($1)");
+}
+
+// Evaluate API
 app.get("/calculate", (req, res) => {
-    const { expression } = req.query;
+    let { expression } = req.query;
     if (!expression) {
         return res.status(400).json({ error: "Expression required" });
     }
 
     try {
+        expression = formatExpression(expression); // Replace √ with sqrt()
         const result = math.evaluate(expression);
         res.json({ expression, result });
     } catch (err) {
